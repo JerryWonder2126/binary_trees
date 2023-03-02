4ad83cc7c743adca7c4cb5a975a472189cf8188d
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-02 20:09:27 +0000

    Add function to get highest number of level down to a leaf from a node

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - calculates the highest number of levels down to a
+ *                      leaf from a node
+ *
+ * @tree: the node to be checked
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (left > right ? left : right);
+}
