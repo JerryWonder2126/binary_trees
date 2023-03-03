6b54e4f9839867864798ba8939f96c5b8f8e4065
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-03 15:31:05 +0000

    Add function to get the sibling of a node

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - gets the sibling of a node
+ *
+ * @node: the node to be checked
+ * Return: the sibling of the node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	return ((node->parent->left->n == node->n)
+		? node->parent->right : node->parent->left);
+}
