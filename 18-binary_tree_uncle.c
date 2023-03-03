95e1f291495888cb4ae3b45e29bc85fef0bf01e5
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-03 15:34:42 +0000

    Add function tó get the uncle of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - gets the uncle of a node
+ *
+ * @node: the node to be checked
+ * Return: the sibling of the node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
