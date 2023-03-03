862ad4aca61b548f244db0eeeb28c4e37e9331cc
Jerry Wonder <jerrycul2001@gmail.com>
2023-03-03 12:13:25 +0000

    Add function to calculate the number of roots down a node

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - calculates the number of roots from a node
+ *
+ * @tree: the node to be checked
+ * Return: 0 if tree is NULL else number of nodes
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL || (tree->right == NULL && tree->left == NULL))
+		return (0);
+
+	size += 1;
+	size += binary_tree_leaves(tree->left);
+	size += binary_tree_leaves(tree->right);
+
+	return (size);
+}
